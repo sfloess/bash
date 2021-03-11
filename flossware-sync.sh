21ccebde017a75ea270c83fca2c065b282febe64
Flossy <sfloess@redhat.com>
2021-03-10 21:53:38 -0500

    Latest

--- flossware-sync.sh
+++ flossware-sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#rsync -av --delete --exclude .git `dirname ${BASH_SOURCE[0]}`/* bedroom-ap:/opt/exports/FlossWare/public/
+rsync -av --delete --exclude .git `dirname ${BASH_SOURCE[0]}`/* bedroom-ap:/opt/exports/FlossWare/public/
 
-mkdir -p /opt/exports/FlossWare/public
-rsync -av --delete --exclude .git `dirname ${BASH_SOURCE[0]}`/* /opt/exports/FlossWare/public/
+#mkdir -p /opt/exports/FlossWare/public
+#rsync -av --delete --exclude .git `dirname ${BASH_SOURCE[0]}`/* /opt/exports/FlossWare/public/

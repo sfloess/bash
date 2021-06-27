810ab5edee0964d2d20249485365bf012098ccaf
Flossy <sfloess@redhat.com>
2021-06-27 04:20:14 -0400

    Latest

--- Host/redhat-workstation/backups.sh
+++ Host/redhat-workstation/backups.sh
@@ -19,17 +19,17 @@ echo "Start Date:           `date +'%F %a'`"
 
 # -----------------------------------------------------------------------------------------
 
-mkdir -p /mnt/cloud-ap/exports/backups/remote/host/redhat-workstation/
+mkdir -p /mnt/server-ap/exports/backups/remote/host/redhat-workstation/
 
-echo "    redhat-workstation -> cloud-ap:"
+echo "    redhat-workstation -> server-ap:"
 echo "        /etc:                        `date +'%H:%M:%S'`"
-rsync -av $1 /etc /mnt/cloud-ap/exports/backups/remote/host/redhat-workstation/ --log-file=${LOG_FILE} $* &> /dev/null
+rsync -av $1 /etc /mnt/server-ap/exports/backups/remote/host/redhat-workstation/ --log-file=${LOG_FILE} $* &> /dev/null
 
 #mkdir -p /mnt/desktop-ap/exports/backups/local/host/redhat-workstation/
 
 echo "        /home:                       `date +'%H:%M:%S'`"
-#rsync -av $1 /home /mnt/cloud-ap/exports/backups/local/host/redhat-workstation/ --log-file=${LOG_FILE} $* &> /dev/null
-rsync -av $1 /home /mnt/cloud-ap/exports/backups/local/host/redhat-workstation/ --log-file=${LOG_FILE} $* &> /dev/null
+#rsync -av $1 /home /mnt/server-ap/exports/backups/local/host/redhat-workstation/ --log-file=${LOG_FILE} $* &> /dev/null
+rsync -av $1 /home /mnt/server-ap/exports/backups/local/host/redhat-workstation/ --log-file=${LOG_FILE} $* &> /dev/null
 
 # -----------------------------------------------------------------------------------------
 
